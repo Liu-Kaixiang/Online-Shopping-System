c87f476c9f64ea6d69f86bf10ce042b2d8d5b872
Liu-Kaixiang <65208161+Liu-Kaixiang@users.noreply.github.com>
2021-12-24 16:54:18 +0800

    Update config.py

--- shopping-more-master(12.23 更新)/config.py
+++ shopping-more-master(12.23 更新)/config.py
@@ -1,6 +1,6 @@
 DATABASE_CONFIG = {
     'Driver': 'ODBC Driver 17 for SQL Server',
-    'Server': 'localhost',
+    'Server': '10.46.142.165',
     'Database': 'OSS',
     'UID': 'sa',
     'Password': '123456'
